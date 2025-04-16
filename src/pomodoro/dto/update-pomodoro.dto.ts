2bb8792420e857f79457d16be1f449d67ed4b939
anantakoirala <ananta.drn2049@gmail.com>
2025-04-16 11:37:08 +0545

    pomodoro update dto changed

--- src/pomodoro/dto/update-pomodoro.dto.ts
+++ src/pomodoro/dto/update-pomodoro.dto.ts
@@ -2,10 +2,10 @@ import { createZodDto } from 'nestjs-zod';
 import { z } from 'zod';
 
 const Schema = z.object({
-  workDuration: z.number().min(1).max(60),
+  workDuration: z.number().min(5).max(60),
   shortBreakDuration: z.number().min(1).max(15),
-  longBreakDuration: z.number().min(1).max(45),
-  longBreakInterval: z.number().min(1).max(10),
+  longBreakDuration: z.number().min(10).max(45),
+  longBreakInterval: z.number().min(2).max(10),
   rounds: z.number().min(1).max(10),
   soundEffect: z.enum([
     'ANALOG',
